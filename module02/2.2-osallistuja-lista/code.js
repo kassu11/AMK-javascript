bec683b8f27b77b8e4f07ca32d1643e188c50c1f
kassu11 <61124141+kassu11@users.noreply.github.com>
2022-11-11 10:15:52 +0200

    fix

--- module02/2.2-osallistuja-lista/code.js
+++ module02/2.2-osallistuja-lista/code.js
@@ -2,7 +2,7 @@
 // After this, the program asks for the names of all participants. 
 // Finally, the program prints the names of the participants on the web page in an ordered list (<ol>) in the same order as they were entered. (2p)
 
-const numberOfNames = prompt("How many participants do you want?");
+const numberOfNames = +prompt("How many participants do you want?");
 
 for(let i = 0; i < numberOfNames; i++) {
   const participantsName = prompt("Give a name");
